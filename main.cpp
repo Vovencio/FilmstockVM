692b73a47b5e63fe764f01107588cc84aebaa5cb
vladimir <83831960+Vovencio@users.noreply.github.com>
2025-11-21 13:10:05 +0100

    Function pointer optimizstion

--- main.cpp
+++ main.cpp
@@ -66,13 +66,13 @@ void addToBuffer(const std::string& s) {
     printBuffer.insert(printBuffer.end(), s.begin(), s.end());
 }
 
-void print(int start, int end) {
-    char* toPrint = getStringFrom(start, end);
+void print(int start, int length, int trash) {
+    char* toPrint = getStringFrom(start, start + length);
     addToBuffer(toPrint);
     delete[] toPrint;
 }
 
-void updateConsole() {
+void updateConsole(int trash, int trash2, int trash3){
     if (!printBuffer.empty()) {
         std::cout.write(printBuffer.data(), static_cast<long>(printBuffer.size()));
         std::cout.flush();
@@ -86,10 +86,10 @@ void addDoubleToBuffer(double value, int precision = 2) {
     addToBuffer(oss.str());
 }
 
-void printNumbers(int start, int end) {
-    for (int i = start; i < end; i++) {
+void printNumbers(int start, int length, int trash) {
+    for (int i = start; i < (start+length); i++) {
         addDoubleToBuffer(film[i]);
-        if (i < end - 1) addToBuffer(" ");
+        if (i < (start+length) - 1) addToBuffer(" ");
     }
 }
 
@@ -97,43 +97,43 @@ void set(int pos, double value) {
     film[pos] = value;
 }
 
-void oppSetNewColor(int pos) {
+void oppSetNewColor(int pos, int trash, int trash2) {
     setNewColor(film[pos]);
 }
 
-void oppSetPosition(int xPos, int yPos) {
+void oppSetPosition(int xPos, int yPos, int trash) {
     setCursorPosition(film[xPos], film[yPos]);
 }
 
-void oppSetTextSize(int pos) {
+void oppSetTextSize(int pos, int trash, int trash2) {
     setTextSize(film[pos]);
 }
 
-void oppDrawTriangles(int pos) {
+void oppDrawTriangles(int pos, int trash, int trash2) {
     int id = static_cast<int>(film[pos]);
 
     drawTriangles(matrix[id]);
 }
 
-void oppDrawText(int pos) {
+void oppDrawText(int pos, int trash, int trash2) {
     int id = static_cast<int>(film[pos]);
 
     drawText(doubleVectorToString(matrix[id]));
 }
 
-void oppBeginDrawing() {
+void oppBeginDrawing(int trash, int trash2, int trash3) {
     beginDrawing();
 }
 
-void oppEndDrawing() {
+void oppEndDrawing(int trash, int trash2, int trash3) {
     endDrawing();
 }
 
-void oppIsPressed(int c, int saveTo) {
+void oppIsPressed(int c, int saveTo, int trash) {
     film[saveTo] = isPressed(static_cast<int>(film[c]));
 }
 
-void mousePressed(int type, int saveTo) {
+void mousePressed(int type, int saveTo, int trash) {
     int t = static_cast<int> (film[type]);
 
     switch (t) {
@@ -148,29 +148,29 @@ void mousePressed(int type, int saveTo) {
     }
 }
 
-void getMouseX(int saveTo) {
+void getMouseX(int saveTo, int trash, int trash2) {
     film[saveTo] = GetMousePosition().x;
 }
 
-void getMouseY(int saveTo) {
+void getMouseY(int saveTo, int trash, int trash2) {
     film[saveTo] = GetMousePosition().y;
 }
 
-void getScroll(int saveTo) {
+void getScroll(int saveTo, int trash, int trash2) {
     film[saveTo] = GetMouseWheelMove();
 }
 
-void getWidth(int saveTo) {
+void getWidth(int saveTo, int trash, int trash2) {
     film[saveTo] = GetScreenWidth();
 }
 
-void getHeight(int saveTo) {
+void getHeight(int saveTo, int trash, int trash2) {
     film[saveTo] = GetScreenHeight();
 }
 
 //region Mathematical operations
 
-void copy(int pos, int saveTo) {
+void copy(int pos, int saveTo, int trash) {
     film[saveTo] = film[pos];
 }
 
@@ -190,27 +190,27 @@ void divide(int pos1, int pos2, int saveTo) {
     film[saveTo] = film[pos1] / film[pos2];
 }
 
-void squareRoot(int pos, int saveTo) {
+void squareRoot(int pos, int saveTo, int trash) {
     film[saveTo] = sqrt(film[pos]);
 }
 
-void sin(int pos, int saveTo) {
+void sin(int pos, int saveTo, int trash) {
     film[saveTo] = sin(film[pos]);
 }
 
-void cos(int pos, int saveTo) {
+void cos(int pos, int saveTo, int trash) {
     film[saveTo] = cos(film[pos]);
 }
 
-void tan(int pos, int saveTo) {
+void tan(int pos, int saveTo, int trash) {
     film[saveTo] = tan(film[pos]);
 }
 
-void asin(int pos, int saveTo) {
+void asin(int pos, int saveTo, int trash) {
     film[saveTo] = asin(film[pos]);
 }
 
-void acos(int pos, int saveTo) {
+void acos(int pos, int saveTo, int trash) {
     film[saveTo] = acos(film[pos]);
 }
 
@@ -234,7 +234,7 @@ void compare(int pos1, int pos2, int saveTo) {
     film[saveTo] = film[pos1] > film[pos2];
 }
 
-void notOp(int pos1, int saveTo) {
+void notOp(int pos1, int saveTo, int trash) {
     film[saveTo] = (film[pos1] == 0) ? 1 : 0;
 }
 
@@ -246,56 +246,56 @@ void orOp(int pos1, int pos2, int saveTo) {
     film[saveTo] = (film[pos1] != 0.0 || film[pos2] != 0.0) ? 1.0 : 0.0;
 }
 
-void jumpIf(int pos1, int jumpTo) {
+void jumpIf(int pos1, int jumpTo, int trash) {
     currentInstruction = (film[pos1] != 0) ? static_cast<int>(film[jumpTo])*4 - STEP :
     currentInstruction;
 }
 
-void iterate(int pos1) {
+void iterate(int pos1, int trash, int trash2) {
     film[pos1] += 1;
 }
 
-void copyFrom(int pos1, int copyTo) {
-    copy(pos1, static_cast<int>(film[copyTo]));
+void copyFrom(int pos1, int copyTo, int trash) {
+    film[pos1] = film[static_cast<int>(film[copyTo])];
 }
 
-void pointer(int pos1, int copyTo) {
+void pointer(int pos1, int copyTo, int trash) {
     film[copyTo] = pos1;
 }
 
-void position(int pos1) {
+void position(int pos1, int trash, int trash2) {
     film[pos1] = currentInstruction;
 }
 
-void jump(int jumpTo) {
+void jump(int jumpTo, int trash, int trash2) {
     currentInstruction = static_cast<int>(film[jumpTo])*4 - STEP;
 }
 
-void end(int getCodeFrom) {
+void end(int getCodeFrom, int trash, int trash2) {
     exitCode = static_cast<int>(film[getCodeFrom]);
     currentInstruction = INT_MAX;
 }
 
-void floor(int pos1, int saveTo) {
+void floor(int pos1, int saveTo, int trash) {
     film[saveTo] = std::floor(film[pos1]);
 }
 
-void round(int pos1, int saveTo) {
+void round(int pos1, int saveTo, int trash) {
     film[saveTo] = std::round(film[pos1]);
 }
 
-void ceil(int pos1, int saveTo) {
+void ceil(int pos1, int saveTo, int trash) {
     film[saveTo] = std::ceil(film[pos1]);
 }
 
-void time(int pos1) {
+void time(int pos1, int trash, int trash2) {
     auto now = std::chrono::system_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
 
     film[pos1] = ms;
 }
 
-void newList(int saveTo) {
+void newList(int saveTo, int trash, int trash2) {
     matrix.push_back(std::vector<double>());
     film[saveTo] = static_cast<double>(matrix.size() - 1);
 }
@@ -304,29 +304,29 @@ void copyFromList(int listId, int pos1, int copyTo) {
     film[copyTo] = matrix[static_cast<int>(film[listId])][static_cast<int>(film[pos1])];
 }
 
-void lengthOfList(int listId, int copyTo) {
+void lengthOfList(int listId, int copyTo, int trash) {
     film[copyTo] = static_cast<double>(matrix[static_cast<int>(film[listId])].size());
 }
 
-void listAmount(int copyTo) {
+void listAmount(int copyTo, int trash, int trash2) {
     film[copyTo] = static_cast<double>(matrix.size());
 }
 
-void addList(int listId, int pos1) {
+void addList(int listId, int pos1, int trash) {
     matrix[static_cast<int>(film[listId])].push_back(film[pos1]);
 }
 
-void removeAt(int listId, int pos1) {
+void removeAt(int listId, int pos1, int trash) {
     int lId = static_cast<int>(film[listId]);
 
     matrix[lId].erase(matrix[lId].begin() + static_cast<int>(film[pos1]));
 }
 
-void emptyList(int listId) {
+void emptyList(int listId, int trash, int trash2) {
     matrix[static_cast<int>(film[listId])].clear();
 }
 
-void removeAll() {
+void removeAll(int trash, int trash2, int trash3) {
     matrix.clear();
 }
 
@@ -343,11 +343,11 @@ void addAt(int listId, int pos2, int pos1) {
 }
 
 
-void reverseList(int listId) {
+void reverseList(int listId, int trash, int trash2) {
     std::reverse(matrix[static_cast<int>(film[listId])].begin(), matrix[static_cast<int>(film[listId])].end());
 }
 
-void shuffleList(int listId) {
+void shuffleList(int listId, int trash, int trash2) {
     int lId = static_cast<int>(film[listId]);
 
     std::random_device rd;
@@ -356,7 +356,7 @@ void shuffleList(int listId) {
 }
 
 
-void sort(int listId, int type) {
+void opp_sort(int listId, int type, int trash) {
     int lId = static_cast<int>(film[listId]);
 
     if (film[type] == 0) {
@@ -368,12 +368,12 @@ void sort(int listId, int type) {
 }
 
 
-void sleep(int msPos) {
+void sleep(int msPos, int trash, int trash2) {
     std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(film[msPos])));
 }
 
 
-void random(int saveTo) {
+void random(int saveTo, int trash, int trash2) {
     std::random_device rd;               // hardware/random entropy source
     std::uniform_real_distribution<> dis(0.0, 1.0);
     film[saveTo] = dis(rd);             // generate a value in [0,1)
@@ -383,13 +383,13 @@ void listSet(int listId, int index, int setTo) {
     matrix[static_cast<int>(film[listId])][static_cast<int>(film[index])] = film[setTo];
 }
 
-void printVector(int p1) {
+void printVector(int p1, int trash, int trash2) {
     for (double d : matrix[static_cast<int>(film[p1])]) {
         addToBuffer(doubleToChar(d));
     }
 }
 
-void printVectorNumbers(int p1) {
+void printVectorNumbers(int p1, int trash, int trash2) {
     addToBuffer("[");
     std::vector<double> vec = matrix[static_cast<int>(film[p1])];
     for (size_t i = 0; i < vec.size(); ++i) {
@@ -417,84 +417,102 @@ void printVectorSeparated(int p1, int p2, int p3) {
 
 //endregion
 
+typedef void (*Operation)(int, int, int) ;
+
+Operation get_operation_from_id(const int id) {
+    switch (id) {
+        case 0:  return copy;
+        case 1:  return add;
+        case 2:  return subtract;
+        case 3:  return multiply;
+        case 4:  return divide;
+        case 5:  return squareRoot;
+        case 6:  return sin;
+        case 7:  return cos;
+        case 8:  return tan;
+        case 9:  return asin;
+        case 10: return acos;
+        case 11: return atan2;
+        case 12: return pow;
+        case 13: return mod;
+        case 14: return equal;
+        case 15: return compare;
+        case 16: return notOp;
+        case 17: return andOp;
+        case 18: return orOp;
+        case 19: return jumpIf;
+        case 20: return print;
+        case 21: return printNumbers;
+        case 22: return iterate;
+        case 23: return copyFrom;
+        case 24: return pointer;
+        case 25: return position;
+        case 26: return jump;
+        case 27: return end;
+        case 28: return floor;
+        case 29: return round;
+        case 30: return ceil;
+        case 31: return time;
+        case 32: return newList;
+        case 33: return copyFromList;
+        case 34: return lengthOfList;
+        case 35: return listAmount;
+        case 36: return addList;
+        case 37: return removeAt;
+        case 38: return emptyList;
+        case 39: return removeAll;
+        case 40: return addAt;
+        case 41: return reverseList;
+        case 42: return shuffleList;
+        case 43: return opp_sort;
+        case 44: return sleep;
+        case 45: return random;
+            // TODO: USE CODE 46
+        case 46: return copy;
+            // TODO: USE CODE 46
+        case 47: return listSet;
+        case 48: return updateConsole;
+        case 49: return printVector;
+        case 50: return printVectorNumbers;
+        case 51: return printVectorSeparated;
+        case 52: return oppSetNewColor;
+        case 53: return oppSetPosition;
+        case 54: return oppSetTextSize;
+        case 55: return oppDrawTriangles;
+        case 56: return oppDrawText;
+        case 57: return oppBeginDrawing;
+        case 58: return oppEndDrawing;
+        case 59: return oppIsPressed;
+        case 60: return mousePressed;
+        case 61: return getMouseX;
+        case 62: return getMouseY;
+        case 63: return getScroll;
+        case 64: return getWidth;
+        case 65: return getHeight;
+
+        default:
+            printf("Unknown instruction type: %d\n", id);
+            return nullptr;
+    }
+}
+
+const int opp_amount = 66;
+
+Operation opp_arr[opp_amount];
+
+void setup_op_arr() {
+    for (int i = 0; i < opp_amount; ++i) {
+        opp_arr[i] = get_operation_from_id(i);
+    }
+}
+
 void execute() {
     int type = instructions[currentInstruction];
     int p1 = instructions[currentInstruction + 1];
     int p2 = instructions[currentInstruction + 2];
     int p3 = instructions[currentInstruction + 3];
 
-    switch (type) {
-        case 0: copy(p1, p2); break;
-        case 1: add(p1, p2, p3); break;
-        case 2: subtract(p1, p2, p3); break;
-        case 3: multiply(p1, p2, p3); break;
-        case 4: divide(p1, p2, p3); break;
-        case 5: squareRoot(p1, p2); break;
-        case 6: sin(p1, p2); break;
-        case 7: cos(p1, p2); break;
-        case 8: tan(p1, p2); break;
-        case 9: asin(p1, p2); break;
-        case 10: acos(p1, p2); break;
-        case 11: atan2(p1, p2, p3); break;
-        case 12: pow(p1, p2, p3); break;
-        case 13: mod(p1, p2, p3); break;
-        case 14: equal(p1, p2, p3); break;
-        case 15: compare(p1, p2, p3); break;
-        case 16: notOp(p1, p2); break;
-        case 17: andOp(p1, p2, p3); break;
-        case 18: orOp(p1, p2, p3); break;
-        case 19: jumpIf(p1, p2); break;
-        case 20: print(p1, p2 + p1); break;
-        case 21: printNumbers(p1, p2 + p1); break;
-        case 22: iterate(p1); break;
-        case 23: copyFrom(p1, p2); break;
-        case 24: pointer(p1, p2); break;
-        case 25: position(p1); break;
-        case 26: jump(p1); break;
-        case 27: end(p1); break;
-        case 28: floor(p1, p2); break;
-        case 29: round(p1, p2); break;
-        case 30: ceil(p1, p2); break;
-        case 31: time(p1); break;
-        case 32: newList(p1); break;
-        case 33: copyFromList(p1, p2, p3); break;
-        case 34: lengthOfList(p1, p2); break;
-        case 35: listAmount(p1); break;
-        case 36: addList(p1, p2); break;
-        case 37: removeAt(p1, p2); break;
-        case 38: emptyList(p1); break;
-        case 39: removeAll(); break;
-        case 40: addAt(p1, p2, p3); break;
-        case 41: reverseList(p1); break;
-        case 42: shuffleList(p1); break;
-        case 43: sort(p1, p2); break;
-        case 44: sleep(p1); break;
-        case 45: random(p1); break;
-        case 46: break;
-        case 47: listSet(p1, p2, p3); break;
-        case 48: updateConsole(); break;
-        case 49: printVector(p1); break;
-        case 50: printVectorNumbers(p1); break;
-        case 51: printVectorSeparated(p1, p2, p3); break;
-        case 52: oppSetNewColor(p1); break;
-        case 53: oppSetPosition(p1, p2); break;
-        case 54: oppSetTextSize(p1); break;
-        case 55: oppDrawTriangles(p1); break;
-        case 56: oppDrawText(p1); break;
-        case 57: oppBeginDrawing(); break;
-        case 58: oppEndDrawing(); break;
-        case 59: oppIsPressed(p1, p2); break;
-        case 60: mousePressed(p1, p2); break;
-        case 61: getMouseX(p1); break;
-        case 62: getMouseY(p1); break;
-        case 63: getScroll(p1); break;
-        case 64: getWidth(p1); break;
-        case 65: getHeight(p1); break;
-
-        default:
-            printf("Unknown instruction type: %d\n", type);
-            break;
-    }
+    opp_arr[type](p1, p2, p3);
 
     currentInstruction += STEP;
 }
@@ -516,6 +534,8 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: runner PATH_TO_LOAD_FROM\n";
         return 1;
     }
+
+    setup_op_arr();
 
     const char* path = argv[1];
 
